9ca17643e7c4814b890bb320cf11be2a7a034686
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-17 17:19:06 +0200

    errore console.log in parametri get

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ import axios from 'axios';
 export default {
   data() {
     return { 
+      searchText:'',
+      apiKey: 'c226b46bed5e65374d45ee2f2efb7ee9',
      
     }
   },
@@ -20,7 +22,32 @@ export default {
     AppHeader
   },
   methods: {
-    
+    search(){
+      console.log('cliccato sul bottone', this.searchText)
+      this.searchText = '',
+
+      axios
+      // oppure con il metodo più semplice
+      // .get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + this.searchText) 
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params : {
+          api_key: this.apiKey,
+          query: this.searchText,
+          console.log(this.searchText);
+          console.log(this.api_key)
+        }
+      })
+      console.log(this.searchText);
+      console.log(this.api_key)
+
+
+      .then((res) => {
+
+      });
+
+      console.log(res)
+    }
+   
   }
 }
 </script>
@@ -30,14 +57,43 @@ export default {
     <!-- 3) Utilizzo del componente -->
     <AppHeader />
     
-    <main class="p-t-2 https://app.diagrams.net/">
-      <input type="text" placeholder="Inserisci il tuo film">
-    </main>
+    <div class="d-flex justify-content-between p-20">
+      <div>
+        <form @submit.prevent.enter="search">
+          <input  v-model="searchText" type="text" placeholder=" Insert your movie" class="text-center">
+          <button  type="submit" class="btn btn-secondary">
+            near
+          </button>
+        </form>
+      </div>
+      <div>
+        <input v-model="searchText" type="text" placeholder=" Insert your serie" class="text-center"  > 
+        <button @click.prevent="search" type="submit" class="btn btn-secondary">
+          near
+        </button>
+      </div>
+    </div>
+  
+  
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" >
 @use 'assets/scss/main' as *;
 // Import all of Bootstrap's CSS
 @import "bootstrap/scss/bootstrap";
+div button{
+  width: 100px;
+  margin-left: 10px;
+}
+.p-20{
+  padding: 20px;
+}
+
+
+
+
+
+
+
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,12 +20,7 @@ export default {
 @use '../assets/scss/partials/variables' as *;
 
 header {
-  background-color: $mainBgColor;
   text-align: center;
   padding: 20px 0;
-}
-
-h1 {
-  color: $mainColor;
 }
 </style>
