e46543058921affc280200b7c9d9afd18576f7cc
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-18 20:35:30 +0200

    Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -15,43 +15,91 @@ export default {
       searchText:'',
       apiKey: 'c226b46bed5e65374d45ee2f2efb7ee9',
       movies: [] ,
-      imgFlag: [ 'en.png', ],
-  }
-},
+      series: [] ,
+    
+    }
+  },
   // 2) Dichiarazione del componente
   components: {
     AppHeader
   },
   methods: {
     search(){
-      // oppure con il metodo più semplice
-      axios
-      .get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey+'&query=' + this.searchText) 
+      // axios
+      // .get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + '&query=' + this.searchText) 
+   
+      // .then((res) => {
+      //   console.log(res.data)
+      //   this.movies = res.data.results;
+      // });
+      // this.searchText = '';
 
-    //   axios
-    //   .get('https://api.themoviedb.org/3/search/movie', {
-    //     params : {
-    //       api_key: this.apiKey,
-    //       query: this.searchText,
-    //     }
-    // })
+      axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params : {
+          api_key: this.apiKey,
+          query: this.searchText,
+        }
+      })
       .then((res) => {
         this.movies = res.data.results;
-      });
+      })
+        // this.searchText = '';
+      
 
-      this.searchText = '';
-    },
-  }
-  ,
-  computed:{
-    changeToFlag(() => {
-      if (this.movies.original_language = 'en')
-      return this.imgFlag[en.png]
-  
+      axios
 
-    })
-  }
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params : {
+          api_key: this.apiKey,
+          query: this.searchText,
+        }
+      })
+      .then((res) => {
+        this.series = res.data.results;
+      })
+        // this.searchText = '';
+      },
+    
+
+    // changeToFlag(lang){
+    //   console.log(lang)
+    //   const validLangs = [
+    //     'en',
+    //     'it',
+    //     'ja',
+    //   ];
+
+    //   if (validLangs.includes(lang)) {
+    //     if (lang == 'ja'){
+    //       return '/img/jp.png';
+    //     }
+    //     else {
+    //       return '/img/' + lang + '.png';
+    //     }
+    //   }
+    //   else {
+    //       return '/img/gm.png';
+    //     }
+    // },
+
+    // oppure
+    getFlagWithObj(lang){
+      const validLangs = {
+        en: 'en.png',
+        it: 'it.png',
+        ja: 'jp.png',
+      };
+      if(lang in validLangs){
+        return '/img/' + validLangs[lang];
+      }
+      else {
+        return '/img/gm.png';
+      }
+    }
+  },
 }
+
 </script>
 
 <template>
@@ -68,15 +116,15 @@ export default {
           </button>
         </form>
       </div>
-      <!-- <div>
+      <div>
         <input v-model="searchText" type="text" placeholder=" Insert your serie" class="text-center"  > 
         <button @click.prevent="search()" type="submit" class="btn btn-secondary">
           near
         </button>
-      </div> -->
+      </div>
     </div>
-
-    <div>
+<div class="d-flex justify-content-between p-20">
+  <div>
       <ol>
         <li v-for="(movie ,i) in movies" :key="i">
           <ul>
@@ -86,8 +134,9 @@ export default {
             <li>
               Titolo Originale: {{ movie.original_title }}
             </li>
+            <!-- sostituiamo la lingua originale da stringa ad una immagine atraverso una funzione che avrà come paramentro la lingua originale  -->
             <li>
-              Lingua: {{ changeToFlag }}
+              Lingua: <img :src="getFlagWithObj(movie.original_language)" alt="bandiera">
             </li>
             <li>
               Voto: {{ movie.vote_average }}
@@ -97,6 +146,32 @@ export default {
         </li>
       </ol>
     </div>
+
+    
+    <div>
+      <ol>
+        <li v-for="(serie ,i) in series" :key="i">
+          <ul>
+            <li >
+              Titolo : {{ serie.name }}
+            </li>
+            <li>
+              Titolo Originale: {{ serie.original_name }}
+            </li>
+            <!-- sostituiamo la lingua originale da stringa ad una immagine atraverso una funzione che avrà come paramentro la lingua originale  -->
+            <li>
+              Lingua: <img :src="getFlagWithObj(serie.original_language)" alt="bandiera">
+            </li>
+            <li>
+              Voto: {{ serie.vote_average }}
+            </li>
+            <hr>
+          </ul>
+        </li>
+      </ol>
+    </div>
+</div>
+
   
   
   </div>
@@ -113,11 +188,5 @@ div button{
 .p-20{
   padding: 20px;
 }
-
-
-
-
-
-
 
 </style>
